4e14609d84c5a02e80d8fc7ff419c0fc491bf626
yangfeng <yangfeng19980715@gmail.com>
2022-07-18 22:18:28 +0800

    Test my first commit in my personal computer legion.

---
+++ chapter4/variadic_templates_4.1.cpp
@@ -0,0 +1,23 @@
+//
+// Created by yangfeng on 2022/7/18.
+//
+#include "../headers.h"
+
+using namespace std;
+
+namespace ch4_1 {
+
+    class Tmp {
+    public:
+    
+    };
+
+}
+
+int
+main()
+{
+
+    return 0;
+}
+
---
+++ headers.h
@@ -0,0 +1,11 @@
+#pragma onec
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <vector>
